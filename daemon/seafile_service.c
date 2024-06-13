041f2e5f677ee37ac7198ba800c77e3e0f548208
lee <loyalpartner@163.com>
2024-06-13 18:53:50 +0800

    Add Repo structure and related service methods to Seafile Thrift API
    - Defined a new structure `Repo` with the following fields:
    - Added new service methods to the `Seafile` Thrift API:
      - `get_repo` to retrieve a repo by `repo_id`
      - `destroy_repo` to delete a repo by `repo_id`
      - `get_repo_list` to retrieve a list of repos with pagination
    - Added methods for configuration management:
      - `set_config` to set a configuration value by key
      - `get_config` to retrieve a configuration value by key
    
    This commit enhances the Seafile Thrift API by allowing more comprehensive interactions with repository data and configuration management.

--- daemon/seafile_service.c
+++ daemon/seafile_service.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
 #include "seafile-rpc.h"
 #include "seafile_service.h"
 
 G_DEFINE_TYPE (SeafileServiceHandler,
               seafile_service_handler,
               TYPE_SEAFILE_HANDLER);
+
+static gboolean
+handle_error(GError **error) {
+    if (error != NULL && *error != NULL) {
+        return FALSE;
+    }
+    return TRUE;
+}
 
 static gboolean
 seafile_service_handler_ping (SeafileIf *iface,
@@ -16,6 +23,29 @@ seafile_service_handler_ping (SeafileIf *iface,
   puts ("Ping()");
 
   return TRUE;
+}
+
+gboolean 
+seafile_service_set_config (SeafileIf *iface,
+                            gint32* _return,
+                            const gchar * key,
+                            const gchar * value,
+                            GError **error)
+{
+  *_return = seafile_set_config(key, value, error);
+
+  return handle_error(error);
+}
+
+
+gboolean seafile_service_get_config (SeafileIf *iface, 
+    gchar ** _return, 
+    const gchar * key, 
+    GError **error)
+{
+  *_return = seafile_get_config(key, error);
+
+  return handle_error(error);
 }
 
 static gboolean 
@@ -38,7 +68,7 @@ seafile_service_handler_download (SeafileIf *iface,
       error
       );
 
-  return TRUE;
+  return handle_error(error);
 }
 
 static gboolean
@@ -61,7 +91,83 @@ seafile_service_handler_clone(SeafileIf *iface,
       request->more_info,
       error);
 
-  return TRUE;
+  return handle_error(error);
+}
+
+static void
+convert_repo (GObject *repo, Repo *r)
+{
+  g_object_get (repo, "id", &r->id, "name", &r->name,
+      "desc", &r->desc, "encrypted", &r->encrypted,
+      "magic", &r->magic, "enc_version", &r->enc_version,
+      "head_cmmt_id", &r->head_cmmt_id,
+      "root", &r->root,
+      "version", &r->version, "last_modify", &r->last_modify,
+      NULL);
+
+  g_object_get (repo,
+      "repo_id", &r->id, "repo_name", &r->name,
+      "repo_desc", &r->desc, "last_modified", &r->last_modify,
+      NULL);
+
+  g_object_get (repo, "worktree", &r->worktree,
+      "relay-id", &r->relay_id,
+      "worktree-invalid", &r->worktree_invalid,
+      "last-sync-time", &r->last_sync_time,
+      "auto-sync", &r->auto_sync,
+      NULL);
+}
+
+
+static gboolean
+seafile_service_handler_get_repo (SeafileIf *iface,
+    Repo **_return,
+    const gchar * repo_id,
+    GError **error)
+{
+  GObject *repo = seafile_get_repo(repo_id, error);
+
+  if (repo == NULL) {
+    return FALSE;
+  }
+  convert_repo(repo, *_return);;
+
+  return handle_error(error);
+}
+
+static gboolean
+seafile_service_handler_destroy_repo (SeafileIf *iface,
+    const gchar * repo_id,
+    GError **error)
+{
+  seafile_destroy_repo(repo_id, error);
+  return handle_error(error);
+}
+
+static gboolean
+seafile_service_handler_shutdown (SeafileIf *iface,
+    GError **error)
+{
+  seafile_shutdown(error);
+  return handle_error(error);
+}
+
+static gboolean
+seafile_service_handler_get_repo_list (SeafileIf *iface,
+    GPtrArray **_return,
+    const gint32 start,
+    const gint32 limit,
+    GError **error)
+{
+  GList *repos = seafile_get_repo_list(start, limit, error);
+  GList *l;
+  for (l = repos; l != NULL; l = l->next) {
+    Repo *r = g_object_new(TYPE_REPO, NULL);
+    convert_repo(l->data, r);
+    g_ptr_array_add(*_return, r);
+  }
+
+  return handle_error(error);
 }
 
 static void
@@ -74,8 +180,6 @@ seafile_service_handler_init (SeafileServiceHandler *self)
 
 }
 
-
-/* TutorialCalculatorHandler's instance finalizer (destructor) */
 static void
 seafile_service_handler_finalize (GObject *object)
 {
@@ -102,11 +206,21 @@ seafile_service_handler_class_init (SeafileServiceHandlerClass *klass)
   gobject_class->finalize = seafile_service_handler_finalize;
 
   /* Register our implementations of CalculatorHandler's methods */
-  seafile_handler_class->ping =
-    seafile_service_handler_ping;
+  seafile_handler_class->ping = seafile_service_handler_ping;
+  seafile_handler_class->set_config =
+    seafile_service_set_config;
+  seafile_handler_class->get_config =
+    seafile_service_get_config;
   seafile_handler_class->clone =
     seafile_service_handler_clone;
   seafile_handler_class->download =
     seafile_service_handler_download;
-
+  seafile_handler_class->get_repo =
+    seafile_service_handler_get_repo;
+  seafile_handler_class->destroy_repo =
+    seafile_service_handler_destroy_repo;
+  seafile_handler_class->shutdown =
+    seafile_service_handler_shutdown;
+  seafile_handler_class->get_repo_list =
+    seafile_service_handler_get_repo_list;
 }
